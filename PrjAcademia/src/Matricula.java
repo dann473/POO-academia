691289706819ce7456cb9a58dd1fa79f11b20a00
Lucas Daniel <lucasdaniel.csm2020@gmail.com>
2026-04-23 00:14:45 -0300

    Criando as primeira classes.
    Incompleto. Falta metade das classes

---
+++ PrjAcademia/src/Matricula.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Matricula {
+	private int id;
+	private Aluno aluno;
+	private Plano plano;
+	private Instrutor instrutor;
+	private LocalDate dataInicio;
+	private LocalDate dataFim;
+}
